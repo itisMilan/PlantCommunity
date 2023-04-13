250bd8032bd5a39d20a881c5ecaee4621823fd00
itisMilan <abhaymilan8@gmail.com>
2023-04-13 18:19:47 +0530

    Setup The TailwindCSS For The Project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/*.ejs", "./views/partials/*ejs", "./js/main.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
